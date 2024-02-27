ef7fc3ebf0686a580cf03b28b275f29602f18ad8
Virginia Vila <106952548+v-vila@users.noreply.github.com>
2024-02-27 13:03:26 +0100

    feat: android match keystore

--- lib/fastlane/plugin/pf_utils/actions/pf_android_match_keystore_action.rb
+++ lib/fastlane/plugin/pf_utils/actions/pf_android_match_keystore_action.rb
@@ -4,16 +4,33 @@ require 'fastlane_core'
 module Fastlane
   module Actions
     class PfAndroidMatchKeystoreAction < Action
-      def self.run(options)
-        unless options[:env]
-          raise "Environment not provided"
+      def self.run(params)
+        keystore = params[:keystore]
+        type = params[:type]
+
+        if type == 'debug'
+          keystore = ENV['ANDROID_MATCH_DEBUG_KEYSTORE']
+        elsif type == 'release'
+          keystore = ENV['ANDROID_MATCH_RELEASE_KEYSTORE']
+        elsif type
+          raise "Invalid type #{type}. Valid values: debug|release."
         end
 
-        sh("jq '{ \"#{options[:env]}\": .#{options[:env]} }' #{options[:env_file_path]} > environments.tmp.json && mv environments.tmp.json #{options[:env_file_path]}")
+        raise "Missing keystore #{keystore}." unless keystore
+
+        return if !params[:force] && File.exist?(keystore)
+
+        temp_dir = Dir.mktmpdir
+        git_url = ENV['ANDROID_MATCH_URL']
+        git_branch = ENV['ANDROID_MATCH_BRANCH']
+
+        sh("git clone --branch #{git_branch} #{git_url} #{temp_dir}")
+        FileUtils.cp("#{temp_dir}/#{keystore}", '../')
+        FileUtils.rm_rf(temp_dir)
       end
 
       def self.description
-        "Set the environment in the app"
+        "Clone and retrieve keystore from a Git repository"
       end
 
       def self.authors
@@ -22,17 +39,23 @@ module Fastlane
 
       def self.available_options
         [
-          FastlaneCore::ConfigItem.new(key: :env,
-                                       description: "The environment",
+          FastlaneCore::ConfigItem.new(key: :keystore,
+                                       description: "The keystore file name",
                                        optional: false),
-          FastlaneCore::ConfigItem.new(key: :env_file_path,
-                                       description: "Path to the environment file",
-                                       optional: false)
+          FastlaneCore::ConfigItem.new(key: :type,
+                                       description: "Type of keystore (debug/release)",
+                                       optional: true,
+                                       default_value: "debug"),
+          FastlaneCore::ConfigItem.new(key: :force,
+                                       description: "Force clone even if keystore exists locally",
+                                       optional: true,
+                                       is_string: false,
+                                       default_value: false)
         ]
       end
 
       def self.is_supported?(platform)
-        true
+        platform == :android
       end
     end
   end
