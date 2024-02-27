15ceb5acf8f9ee449f98c0fda421dd89153d2757
Virginia Vila <106952548+v-vila@users.noreply.github.com>
2024-02-27 12:57:04 +0100

    feat: new version

---
+++ lib/fastlane/plugin/pf_utils/actions/pf_android_match_keystore_action.rb
@@ -0,0 +1,39 @@
+require 'fastlane/action'
+require 'fastlane_core'
+
+module Fastlane
+  module Actions
+    class PfAndroidMatchKeystoreAction < Action
+      def self.run(options)
+        unless options[:env]
+          raise "Environment not provided"
+        end
+
+        sh("jq '{ \"#{options[:env]}\": .#{options[:env]} }' #{options[:env_file_path]} > environments.tmp.json && mv environments.tmp.json #{options[:env_file_path]}")
+      end
+
+      def self.description
+        "Set the environment in the app"
+      end
+
+      def self.authors
+        ["Your Name"]
+      end
+
+      def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(key: :env,
+                                       description: "The environment",
+                                       optional: false),
+          FastlaneCore::ConfigItem.new(key: :env_file_path,
+                                       description: "Path to the environment file",
+                                       optional: false)
+        ]
+      end
+
+      def self.is_supported?(platform)
+        true
+      end
+    end
+  end
+end
--- lib/fastlane/plugin/pf_utils/version.rb
+++ lib/fastlane/plugin/pf_utils/version.rb
@@ -1,5 +1,5 @@
 module Fastlane
   module PfUtils
-    VERSION = "0.1.2"
+    VERSION = "0.1.3"
   end
 end
