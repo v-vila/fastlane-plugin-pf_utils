5ffa459d6c3d43255b19029fa0266d384c903095
Virginia Vila <106952548+v-vila@users.noreply.github.com>
2024-02-28 12:30:46 +0100

    feat: version

--- lib/fastlane/plugin/pf_utils/actions/pf_nexus_upload_as_zip.action.rb
+++ lib/fastlane/plugin/pf_utils/actions/pf_nexus_upload_as_zip.action.rb
@@ -8,16 +8,16 @@ module Fastlane
           artifact_extension = File.extname(params[:artifact]).shellescape[1..-1]
           outputFilePath = "#{params[:artifact]}.zip"
           sh("zip -r #{outputFilePath} #{params[:artifact]}")
-          classifier = params[:classifier] ? "#{params[:classifier]}-#{artifact_extension}" : artifact_extension
+          classifier = params[:repo_classifier] ? "#{params[:repo_classifier]}-#{artifact_extension}" : artifact_extension
 
-          nexus_upload(
-            nexus_version: params[:nexus_version].to_s,
+          other_action.nexus_upload(
+            nexus_version: params[:nexus_version],
             mount_path: params[:mount_path],
             file: outputFilePath,
             repo_id: params[:repo_id],
             repo_group_id: params[:repo_group_id],
             repo_project_name: params[:repo_group_id],
-            repo_project_version: params[:version],
+            repo_project_version: params[:repo_project_version],
             repo_classifier: classifier,
             endpoint: params[:endpoint],
             username: params[:username],
@@ -25,15 +25,15 @@ module Fastlane
             verbose: params[:verbose]
           )
         else
-          nexus_upload(
-            nexus_version: params[:nexus_version].to_s,
+          other_action.nexus_upload(
+            nexus_version: params[:nexus_version],
             mount_path: params[:mount_path],
             file: params[:artifact],
             repo_id: params[:repo_id],
             repo_group_id: params[:repo_group_id],
             repo_project_name: params[:repo_project_name],
-            repo_project_version: params[:version],
-            repo_classifier: params[:classifier],
+            repo_project_version: params[:repo_project_version],
+            repo_classifier: params[:repo_classifier],
             endpoint: params[:endpoint],
             username: params[:username],
             password: params[:password],
@@ -55,11 +55,15 @@ module Fastlane
           FastlaneCore::ConfigItem.new(key: :artifact,
                                        description: "Path to the artifact to upload",
                                        optional: false),
-          FastlaneCore::ConfigItem.new(key: :version,
-                                       description: "Version of the artifact",
+          FastlaneCore::ConfigItem.new(key: :nexus_version,
+                                       description: "Nexus Version",
+                                       is_string: false
                                        optional: false),
-          FastlaneCore::ConfigItem.new(key: :classifier,
-                                       description: "Classifier for the artifact",
+          FastlaneCore::ConfigItem.new(key: :repo_project_version,
+                                       description: "Repo project version",
+                                       optional: false),
+          FastlaneCore::ConfigItem.new(key: :repo_classifier,
+                                       description: "Repo classifier for the artifact",
                                        optional: true,
                                        default_value: nil),
           FastlaneCore::ConfigItem.new(key: :as_zip,
