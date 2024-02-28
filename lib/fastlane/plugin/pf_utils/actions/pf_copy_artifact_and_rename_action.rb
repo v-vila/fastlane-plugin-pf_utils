b73d40cd7815987ab451defb489ae6ae67d96826
Virginia Vila <106952548+v-vila@users.noreply.github.com>
2024-02-28 11:23:51 +0100

    feat: pf nexus upload as zip action

--- lib/fastlane/plugin/pf_utils/actions/pf_copy_artifact_and_rename_action.rb
+++ lib/fastlane/plugin/pf_utils/actions/pf_copy_artifact_and_rename_action.rb
@@ -1,52 +1,53 @@
 require 'fastlane/action'
-require_relative '../helper/pf_copy_artifact_and_rename_helper'
 
 module Fastlane
   module Actions
-    class PfCopyAndRenameArtifactsAction < Action
-      def self.run(options)
-        require 'fastlane/actions/shared_values' # Add this require statement
+    class PfCopyCustomArtifactsAction < Action
+      def self.run(params)
+        app_name = params[:app_name]
+        artifact_path = params[:artifact_path]
+        dist_artifacts_path = params[:dist_artifacts_path]
+        default_dir_relative_to_workspace = params[:default_dir_relative_to_workspace]
 
-        gradle(project_dir: 'android', task: options[:task], build_type: options[:build_type], flavor: options[:flavor] || nil)
-        artifact_path = options[:build_type] == 'Debug' ? lane_context[SharedValues::GRADLE_APK_OUTPUT_PATH] : lane_context[SharedValues::GRADLE_AAB_OUTPUT_PATH]
-        default_dir_relative_to_workspace = 'dist/artifacts/mets'
-
-        if options[:app_name]
-          target_path = options[:dist_artifacts_path] || "../../../#{default_dir_relative_to_workspace}"
+        if app_name
+          target_path = dist_artifacts_path || "../../../#{default_dir_relative_to_workspace}"
           artifact_extension = File.extname(artifact_path).shellescape
-          new_artifact_name = "#{options[:app_name]}-#{get_app_version}#{artifact_extension}"
+          new_artifact_name = "#{app_name}-#{get_app_version()}#{artifact_extension}"
           sh("mkdir -p #{target_path}")
           sh("cp \"#{artifact_path}\" \"#{target_path}/#{new_artifact_name}\"")
         else
-          target_path = options[:dist_artifacts_path] || "../../#{default_dir_relative_to_workspace}"
+          target_path = dist_artifacts_path || "../../#{default_dir_relative_to_workspace}"
           copy_artifacts(target_path: target_path, artifacts: [artifact_path])
         end
       end
 
       def self.description
-        "Copy and rename artifacts android"
+        "Copies custom artifacts with optional renaming based on provided parameters"
       end
 
       def self.authors
-        ["Your Name"]
+        ["Plataforma SDM"]
       end
 
       def self.available_options
-        require 'fastlane_core/config_item' # Add this require statement
-
         [
           FastlaneCore::ConfigItem.new(key: :app_name,
-                                       description: "Name of the app",
+                                       description: "App name",
                                        optional: true),
+          FastlaneCore::ConfigItem.new(key: :artifact_path,
+                                       description: "Path to the artifact",
+                                       optional: false),
           FastlaneCore::ConfigItem.new(key: :dist_artifacts_path,
-                                       description: "Path to the destination directory for artifacts",
-                                       optional: true)
-          # Add other options as needed
+                                       description: "Path to the distribution artifacts",
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :default_dir_relative_to_workspace,
+                                       description: "Default directory relative to workspace",
+                                       optional: false)
         ]
       end
 
       def self.is_supported?(platform)
-        platform == :android
+        true
       end
     end
   end
--- lib/fastlane/plugin/pf_utils/actions/pf_nexus_upload_aszip.action.rb
+++ lib/fastlane/plugin/pf_utils/actions/pf_nexus_upload_aszip.action.rb
@@ -2,43 +2,110 @@ require 'fastlane/action'
 
 module Fastlane
   module Actions
-    class PfNexusUploadAsZipAction < Action
+    class NexusUploadAction < Action
       def self.run(params)
-        UI.message("The pf_utils plugin is working!")
+        if params[:as_zip]
+          artifact_extension = File.extname(params[:artifact]).shellescape[1..-1]
+          outputFilePath = "#{params[:artifact]}.zip"
+          sh("zip -r #{outputFilePath} #{params[:artifact]}")
+          classifier = params[:classifier] ? "#{params[:classifier]}-#{artifact_extension}" : artifact_extension
+
+          nexus_upload(
+            nexus_version: params[:nexus_version],
+            mount_path: params[:mount_path],
+            file: outputFilePath,
+            repo_id: params[:repo_id],
+            repo_group_id: params[:repo_group_id],
+            repo_project_name: params[:repo_group_id],
+            repo_project_version: params[:version],
+            repo_classifier: classifier,
+            endpoint: params[:endpoint],
+            username: params[:username],
+            password: params[:password],
+            verbose: params[:verbose]
+          )
+        else
+          nexus_upload(
+            nexus_version: params[:nexus_version],
+            mount_path: params[:mount_path],
+            file: params[:artifact],
+            repo_id: params[:repo_id],
+            repo_group_id: params[:repo_group_id],
+            repo_project_name: params[:repo_project_name],
+            repo_project_version: params[:version],
+            repo_classifier: params[:classifier],
+            endpoint: params[:endpoint],
+            username: params[:username],
+            password: params[:password],
+            verbose: params[:verbose]
+          )
+        end
       end
 
       def self.description
-        "pf utils"
+        "Uploads artifact as zip to Nexus repository"
       end
 
       def self.authors
-        ["Plataforma"]
-      end
-
-      def self.return_value
-        # If your method provides a return value, you can describe here what it does
-      end
-
-      def self.details
-        # Optional:
-        "pf utils"
+        ["Plataforma SDM"]
       end
 
       def self.available_options
         [
-          # FastlaneCore::ConfigItem.new(key: :your_option,
-          #                         env_name: "PF_UTILS_YOUR_OPTION",
-          #                      description: "A description of your option",
-          #                         optional: false,
-          #                             type: String)
+          FastlaneCore::ConfigItem.new(key: :artifact,
+                                       description: "Path to the artifact to upload",
+                                       optional: false),
+          FastlaneCore::ConfigItem.new(key: :version,
+                                       description: "Version of the artifact",
+                                       optional: false),
+          FastlaneCore::ConfigItem.new(key: :classifier,
+                                       description: "Classifier for the artifact",
+                                       optional: true,
+                                       default_value: nil),
+          FastlaneCore::ConfigItem.new(key: :as_zip,
+                                       description: "Flag to indicate if artifact should be zipped before upload",
+                                       optional: true,
+                                       default_value: false),
+          FastlaneCore::ConfigItem.new(key: :nexus_version,
+                                       description: "Nexus version",
+                                       optional: true,
+                                       default_value: 3),
+          FastlaneCore::ConfigItem.new(key: :mount_path,
+                                       description: "Mount path",
+                                       optional: true,
+                                       default_value: ''),
+          FastlaneCore::ConfigItem.new(key: :repo_id,
+                                       description: "Repository ID",
+                                       optional: true,
+                                       default_value: 'mobile'),
+          FastlaneCore::ConfigItem.new(key: :repo_group_id,
+                                       description: "Repository group ID",
+                                       optional: true,
+                                       default_value: 'com.mapfre'),
+          FastlaneCore::ConfigItem.new(key: :repo_project_name,
+                                       description: "Repository project name",
+                                       optional: true,
+                                       default_value: 'mapfreapp'),
+          FastlaneCore::ConfigItem.new(key: :endpoint,
+                                       description: "Nexus endpoint URL",
+                                       optional: true,
+                                       default_value: ENV['NEXUS_URL']),
+          FastlaneCore::ConfigItem.new(key: :username,
+                                       description: "Nexus username",
+                                       optional: true,
+                                       default_value: ENV['NEXUS_USER']),
+          FastlaneCore::ConfigItem.new(key: :password,
+                                       description: "Nexus password",
+                                       optional: true,
+                                       default_value: ENV['NEXUS_PASSWORD']),
+          FastlaneCore::ConfigItem.new(key: :verbose,
+                                       description: "Verbose output",
+                                       optional: true,
+                                       default_value: true)
         ]
       end
 
       def self.is_supported?(platform)
-        # Adjust this if your plugin only works for a particular platform (iOS vs. Android, for example)
-        # See: https://docs.fastlane.tools/advanced/#control-configuration-by-lane-and-by-platform
-        #
-        # [:ios, :mac, :android].include?(platform)
         true
       end
     end
